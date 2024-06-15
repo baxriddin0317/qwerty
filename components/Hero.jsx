7e0285babb8eecb615d35ea0c6cd20ca49ed9768
Abdushukur Rasulov <abdushukurrasulov1994@gmail.com>
2024-06-15 21:34:28 +0500

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <section className='px-12 py-36'>
+    <section className='px-8 py-36'>
       <div className='flex flex-col gap-6'>
         <button className='w-full sm:w-64 max-h-16 text-lg border border-brand-white bg-brand-btn text-white rounded-xl p-3'>
           Buy Qwerty
