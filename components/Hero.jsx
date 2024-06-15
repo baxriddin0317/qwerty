cac2eafc6a8a700616a2763dbeb0ea33eb7bc224
Abdushukur Rasulov <abdushukurrasulov1994@gmail.com>
2024-06-15 17:24:53 +0500

    Buttons has been updated -  Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,10 +7,10 @@ export const Hero = () => {
         <button className='sm:w-52 max-h-16 text-lg md:text-xl border border-brand-white bg-brand-white rounded-xl px-3 md:px-8 py-5'>
           Buy Qwerty
         </button>
-        <button className='sm:w-52 max-h-16 text-lg md:text-xl text-brand-white sm:text-black border border-brand-white bg-transparent sm:bg-brand-white rounded-xl px-3 md:px-8 py-5'>
+        <button className='sm:w-52 max-h-16 text-lg md:text-xl border border-brand-white bg-brand-white rounded-xl px-3 md:px-8 py-5'>
         Hire Talents
         </button>
-        <button className='sm:w-52 max-h-16 text-lg md:text-xl text-brand-white sm:text-black border border-brand-white bg-transparent sm:bg-brand-white rounded-xl px-3 md:px-8 py-5'>
+        <button className='sm:w-52 max-h-16 text-lg md:text-xl border border-brand-white bg-brand-white rounded-xl px-3 md:px-8 py-5'>
         Buy E-Books
         </button>
       </div>
