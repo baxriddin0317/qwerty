ed9b50eea786a8b8b336a32be1f14a6eb31c5cae
Abdushukur Rasulov <abdushukurrasulov1994@gmail.com>
2024-06-15 21:48:38 +0500

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -5,7 +5,7 @@ export const About = () => {
     <section className='max-w-6xl md:h-[604px] flex items-center justify-end relative mx-auto px-5 md:px-1.5 py-8 mb-24 md:mb-40'>
       <div className='absolute left-0 opacity-60 z-10 w-56 h-56 md:w-[604px] md:h-[604px] rounded-full bg-brand-red-100 blur-[100px] md:blur-[227.77777099609375px]'></div>
       <div className='relative z-20 text-center sm:text-left sm:pr-12 md:pr-28'>
-        <p className='max-w-[555px] text-white text-4xl md:text-[63.611px] leading-normal capitalize'>
+        <p className='max-w-[555px] text-white text-4xl md:text-[63.611px] leading-normal '>
         Hire talents, <span >buy e-books</span> and pay with cryptos.
         </p>
       </div>
